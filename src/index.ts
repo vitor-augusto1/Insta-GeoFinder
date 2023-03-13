c56835ef2d8def3688d8ee8a9afbfdcc72c7438c
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-03-13 14:55:52 -0300

    feat: Add a private method to add routes

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,10 @@ class App {
     this.express.use(express.json());
     this.express.use(cors());
   }
+
+  private routes() {
+    this.express.use(routes);
+  }
 }
 
 export default new App().express;
