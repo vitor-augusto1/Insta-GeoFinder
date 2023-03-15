6d421a9726c4d74b4bcc8f99799848b4ffc5248f
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-03-15 18:10:18 -0300

    feat: Add new `returnMediaFromAinstagramPlace` method for place media handling

--- src/controllers/instagramPlacesController.ts
+++ src/controllers/instagramPlacesController.ts
@@ -4,6 +4,27 @@ import { Request, Response } from "express";
 import axios from "axios";
 
 class instagramPlacesController {
+  private async returnMediaFromAninstagramPlace(placeID: number = 30630047) {
+    const options = {
+      method: 'POST',
+      url: 'https://rocketapi-for-instagram.p.rapidapi.com/instagram/location/get_media',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': `${process.env.RAPID_API}`,
+        'X-RapidAPI-Host': 'rocketapi-for-instagram.p.rapidapi.com'
+      },
+      data: `{"id":${placeID},"page":null,"max_id":null}`
+    };
+    try {
+      const apiResponse = await axios.request(options);
+      const responseBody = apiResponse.data.response.body;
+      const sections = responseBody.sections;
+      return sections;
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
 }
 
 export default new instagramPlacesController();
