16da97110733e3605d3664f066f687352f9822a1
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-03-31 15:46:21 -0300

    refactor: Move function to main.js and call it in index.html

--- src/public/main.js
+++ src/public/main.js
@@ -5,3 +5,32 @@ function messageToUser(appear, documentObject, elementName, message) {
   }
   loadingHtmlTag.textContent = message;
 }
+
+
+function fetchPlaceHandler(place, document) {
+  fetch(`/api/v1/place-information/?place_id=${place}`)
+    .then(data => { return data.json(); })
+    .then(jsonResponse => {
+      messageToUser(false, document, "message", "")
+      const arrayOfUserMedia = jsonResponse.media;
+      for (let media of arrayOfUserMedia) {
+        const layoutOfContent = media.layout_content;
+        const userMedia = layoutOfContent.medias[0].media;
+        const userPic = userMedia.user.profile_pic_url;
+        const userName = userMedia.user.username;
+        const container = document.getElementById("post-information");
+        const img = document.createElement('img');
+        const UserProfileLink = document.createElement('a');
+        img.crossOrigin = "same-origin";
+        img.src = `https://corsproxy.io/?${userPic}`;
+        UserProfileLink.href = "https://www.instagram.com/"+userName;
+        UserProfileLink.appendChild(img)
+        container.appendChild(UserProfileLink);
+        console.log({userName: userPic});
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      messageToUser(true, document, "message", "Users not found.");
+    });
+}
